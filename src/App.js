d2846ed1bb03ea023ff149c215d3f011599b4ddb
LeeSeulbi <dbdp113@naver.com>
2023-08-03 16:33:14 +0900

    css완성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './index.css';
-import {BsBookFill,BsFillCaretDownFill} from 'react-icons/bs';
+import {BsFillCaretDownFill} from 'react-icons/bs';
 import {FaClipboardList} from 'react-icons/fa';
 import {FaRankingStar} from 'react-icons/fa6';
 import {IoCloseSharp} from 'react-icons/io5';
-import {BiListUl} from 'react-icons/bi';
+import {BiListUl,BiSolidBookAlt} from 'react-icons/bi';
 import Search from './components/Search';
 import Appointment from "./components/Appointment";
 import BookInfo from "./components/BookInfo";
@@ -15,7 +15,7 @@ import bookData from './booklist.json';
 export default function App(){
   return(
     <div id="wrap">
-      <h2><span><BsBookFill /></span>도서 대출 예약 시스템</h2>
+      <h2><span><BiSolidBookAlt /></span>도서 대출 예약 시스템</h2>
       <Search />
       <div id="BookRank">
       <h3><FaRankingStar /> 도서 인기 순위</h3>
