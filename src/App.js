64489173005dc07453b954b643c73f9e5795465b
LeeSeulbi <dbdp113@naver.com>
2023-08-03 17:11:38 +0900

    json파일 보충

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {FaClipboardList} from 'react-icons/fa';
 import {FaRankingStar} from 'react-icons/fa6';
 import {IoCloseSharp} from 'react-icons/io5';
 import {BiListUl,BiSolidBookAlt} from 'react-icons/bi';
+
 import Search from './components/Search';
 import Appointment from "./components/Appointment";
 import BookInfo from "./components/BookInfo";
@@ -18,7 +19,7 @@ export default function App(){
       <h2><span><BiSolidBookAlt /></span>도서 대출 예약 시스템</h2>
       <Search />
       <div id="BookRank">
-      <h3><FaRankingStar /> 도서 인기 순위</h3>
+      <h3><FaRankingStar /> 도서 대출 인기 순위</h3>
       <div className="box">
         <ol>
           <BookRank book={bookData} />
